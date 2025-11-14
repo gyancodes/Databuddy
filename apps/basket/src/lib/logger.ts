f7facc390ed1a2aaeed3c1ce5a4fab0132bce701
iza <59828082+izadoesdev@users.noreply.github.com>
2025-11-14 22:37:59 +0200

    fix: logger

--- apps/basket/src/lib/logger.ts
+++ apps/basket/src/lib/logger.ts
@@ -1,12 +1,3 @@
-// import { logger as sharedLogger } from "@databuddy/shared/logger";
+import { createLogger } from "@databuddy/shared/logger";
 
-// export const logger = sharedLogger.child({
-// 	service: "basket",
-// });
-
-import pino from "pino";
-
-export const logger = pino({
-	level: "debug",
-	name: "basket",
-});
+export const logger = createLogger("basket");
